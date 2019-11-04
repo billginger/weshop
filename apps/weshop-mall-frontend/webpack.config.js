e1d691bb16e5c3f82f18ab407e424d3427e6c16d
Bill <bill@thinkchina.com.au>
2019-11-04 15:42:38 +0800

    Add webpack config file

---
+++ apps/weshop-mall-frontend/webpack.config.js
@@ -0,0 +1,48 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		'js/bundle': './src/App.jsx'
+	},
+	output: {
+		filename: '[name].js?[contenthash]',
+		path: __dirname + '/dist',
+		publicPath: '/'
+	},
+	module: {
+		rules: [{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [
+						['@babel/preset-env', { targets: { ios: 11, android: 9 } }],
+						'@babel/preset-react'
+					],
+					plugins: [
+						['import', { libraryName: 'antd-mobile', style: true }]
+					]
+				}
+			}
+		}, {
+			test: /\.(le|c)ss$/,
+			use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader?javascriptEnabled=1']
+		}]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/style.css?[contenthash]'
+		}),
+		new OptimizeCssAssetsPlugin({
+			cssProcessorPluginOptions: {
+				preset: ['default', { discardComments: { removeAll: true } }]
+			}
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	]
+};
